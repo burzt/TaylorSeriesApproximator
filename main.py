895db8417c78682ea1bb3c8ddd61f432a4c5b87d
Aaron Elam <speztra97@gmail.com>
2020-12-02 05:13:59 +0000

    Error checker done

--- main.py
+++ main.py
@@ -15,11 +15,10 @@
 # and remember, indentation really matters in Python!
 
 import math   # only used for for error checking
-import enum   # enumerated values = set of symbolic names bound to unique, constant values
 
 validFunctionList = ["e^x", "cos", "sin"]
 
-# Finished function for finding factorial of a value
+# Finished function for finding factorial of a value (in case we need it)
 def factorial(n):
   # base case
     if n == 0:
@@ -28,39 +27,53 @@ def factorial(n):
       # recursively multiply n and previous factorial
         return n * factorial(n - 1)
 
-# Function to check error of our approximation
-def errorChecker(type, x, myApprox):
-  if(function == sine):
-    # TODO: real sin(x) calculation here
-    realValue = math.sin(x)
-    #print(abs(realValue - myApprox))
-  if(function == cos):
-    #TODO: real cos(x) calculation here
-    print("no error here baby :)")
-    #print(abs(realValue - myApprox))
-  if(function == e):
-    # TODO: real e^x calculation here
-    print("yo")
-    #print(abs(realValue - myApprox))
+# Function for calculating Taylor series approximation of a given function
+def taylor(function, x):
+  if(function == 'sin'):
+    
+  if(function == 'cos'):
+
+  if(function == 'e^x'):
+    n = 4 #test different values here, could automate this out later
+    for i in range(n):
+      
+  
+
+# Finished function to check error of our approximation
+# Accuracy needs to be within 0.00000001 (7 zeroes)
+def errorChecker(function, x, myApprox):
+  if(function == 'sin'):
+    realValue = math.sin(x)               # does sine
+  if(function == 'cos'):
+    realValue = math.cos(x)               # does cosine
+  if(function == 'e^x'):
+    realValue = math.exp(x)
+  errorValue = abs(realValue - myApprox)  # take absolute value of difference
+  print(f"Accuracy of Taylor sim for {function}: {errorValue}")
 
 
 def main():
-    # This while true loop will keep asking User for input until it is correct
+    # This while true loop will ask for function, then x until user enters both correctly.
     while True:
       try:
         function = input("Enter the function you want: (e^x, cos, sin): ")
         if function in validFunctionList:
-          break
+          ## Ask user for float in range [-5,5]
+          user_x = int(input(f"Enter the x you want to use in {function} (in range [-5,5]): "))
+          if -5 <= user_x <= 5:
+            break # leave while True loop bc function and x are valid
+          print("Enter a valid x between -5 and 5 inclusive.\n")
         print("Enter a valid function such as 'e^x', 'cos', or 'sin' exactly as printed.\n")
       except Exception as e:  # just in case there's a major messup
         print(e)
     # end while true loop
 
-    ## TODO
 
-    # just here to show something
-    f = factorial(5)
-    print(f)
+    # Calculate Taylor series approximation
+    myApprox = taylor(function, user_x)
+
+    # Check error of our Taylor series approximation
+    errorChecker(function, user_x, myApprox)
 
 
 # weird python stuff we gotta have
